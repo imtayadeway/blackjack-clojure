c6a1ddea49fd697ed5c7e52278f3da746f1e0e3a
Tim Wade <hello@timjwade.com>
2016-04-26 21:40:08 -0700

    I don't think this test is adding anything

--- test/blackjack_clojure/core_test.clj
+++ test/blackjack_clojure/core_test.clj
@@ -10,63 +10,6 @@
   (testing "That we have all the suits in a standard deck."
     (is (= #{"♠" "♥" "♦" "♣"} suits))))
 
-(deftest the-full-deck
-  (testing "That we have a full deck."
-    (is (= (set [{:rank "ace", :suit "♣"}
-                 {:rank "ace", :suit "♦"}
-                 {:rank "ace", :suit "♥"}
-                 {:rank "ace", :suit "♠"}
-                 {:rank 2, :suit "♣"}
-                 {:rank 2, :suit "♦"}
-                 {:rank 2, :suit "♥"}
-                 {:rank 2, :suit "♠"}
-                 {:rank 3, :suit "♣"}
-                 {:rank 3, :suit "♦"}
-                 {:rank 3, :suit "♥"}
-                 {:rank 3, :suit "♠"}
-                 {:rank 4, :suit "♣"}
-                 {:rank 4, :suit "♦"}
-                 {:rank 4, :suit "♥"}
-                 {:rank 4, :suit "♠"}
-                 {:rank 5, :suit "♣"}
-                 {:rank 5, :suit "♦"}
-                 {:rank 5, :suit "♥"}
-                 {:rank 5, :suit "♠"}
-                 {:rank 6, :suit "♣"}
-                 {:rank 6, :suit "♦"}
-                 {:rank 6, :suit "♥"}
-                 {:rank 6, :suit "♠"}
-                 {:rank 7, :suit "♣"}
-                 {:rank 7, :suit "♦"}
-                 {:rank 7, :suit "♥"}
-                 {:rank 7, :suit "♠"}
-                 {:rank 8, :suit "♣"}
-                 {:rank 8, :suit "♦"}
-                 {:rank 8, :suit "♥"}
-                 {:rank 8, :suit "♠"}
-                 {:rank 9, :suit "♣"}
-                 {:rank 9, :suit "♦"}
-                 {:rank 9, :suit "♥"}
-                 {:rank 9, :suit "♠"}
-                 {:rank 10, :suit "♣"}
-                 {:rank 10, :suit "♦"}
-                 {:rank 10, :suit "♥"}
-                 {:rank 10, :suit "♠"}
-                 {:rank "jack", :suit "♣"}
-                 {:rank "jack", :suit "♦"}
-                 {:rank "jack", :suit "♥"}
-                 {:rank "jack", :suit "♠"}
-                 {:rank "queen", :suit "♣"}
-                 {:rank "queen", :suit "♦"}
-                 {:rank "queen", :suit "♥"}
-                 {:rank "queen", :suit "♠"}
-                 {:rank "king", :suit "♣"}
-                 {:rank "king", :suit "♦"}
-                 {:rank "king", :suit "♥"}
-                 {:rank "king", :suit "♠"}
-                 ])
-           (set deck)))))
-
 (deftest deck-count
   (testing "That we have 52 cards in the deck."
     (is (= (count deck) 52))))
