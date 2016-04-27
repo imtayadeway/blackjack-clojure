c32b1a61cdbe27b05db562cf74a5a10a956fa50f
Tim Wade <hello@timjwade.com>
2016-04-26 21:18:24 -0700

    Scoring

--- src/blackjack_clojure/core.clj
+++ src/blackjack_clojure/core.clj
@@ -1,8 +1,28 @@
 (ns blackjack-clojure.core)
 
 (def suits #{"♠" "♥" "♦" "♣"})
-(def ranks (concat (range 1 11) ["jack" "queen" "king"]))
+(def ranks (set (concat ["ace"] (range 2 11) ["jack" "queen" "king"])))
 (def deck
-  (into [] (for [s suits
-                r ranks]
-            {:rank r, :suit s})))
+  (into [] (for [suit suits
+                 rank ranks]
+             {:suit suit, :rank rank})))
+
+(defn card-value
+  [card]
+  (let [rank (:rank card)]
+    (case rank
+      (2 3 4 5 6 7 8 9 10) rank
+      ("jack" "queen" "king") 10
+      "ace" 11)))
+
+(defn any-aces? [values] (some #(= 11 %) values))
+
+(defn score-values
+  [values]
+  (let [sorted-values (-> values (sort) (vec))
+        sum (reduce + values)]
+    (if (<= sum 21)
+      sum
+      (if (any-aces? values)
+        (recur (-> sorted-values (pop) (conj 1)))
+        sum))))
--- test/blackjack_clojure/core_test.clj
+++ test/blackjack_clojure/core_test.clj
@@ -4,7 +4,7 @@
 
 (deftest all-the-ranks
   (testing "That we have the full set of ranks in a standard deck."
-    (is (= ranks [1 2 3 4 5 6 7 8 9 10 "jack" "queen" "king"]))))
+    (is (= ranks #{"ace" 2 3 4 5 6 7 8 9 10 "jack" "queen" "king"}))))
 
 (deftest all-the-suits
   (testing "That we have all the suits in a standard deck."
@@ -13,10 +13,10 @@
 (deftest the-full-deck
   (testing "That we have a full deck."
     (is (= (set deck)
-           (set [{:rank 1, :suit "♣"}
-                 {:rank 1, :suit "♦"}
-                 {:rank 1, :suit "♥"}
-                 {:rank 1, :suit "♠"}
+           (set [{:rank "ace", :suit "♣"}
+                 {:rank "ace", :suit "♦"}
+                 {:rank "ace", :suit "♥"}
+                 {:rank "ace", :suit "♠"}
                  {:rank 2, :suit "♣"}
                  {:rank 2, :suit "♦"}
                  {:rank 2, :suit "♥"}
@@ -70,3 +70,37 @@
 (deftest deck-count
   (testing "That we have 52 cards in the deck."
     (is (= (count deck) 52))))
+
+(deftest score-two-twos
+  (testing "That we can score a hand with two twos."
+    (is (= (score-hand [{:rank 2 :suit "♥"} {:rank 2 :suit "♣"}])
+           4))))
+
+(deftest score-two-three
+  (testing "That we can score a hand with a two and a three."
+    (is (= (score-hand [{:rank 2 :suit "♥"} {:rank 3 :suit "♣"}])
+           5))))
+(deftest score-two-three-four
+  (testing "That we can score a hand with three cards."
+    (is (= (score-hand [{:rank 2 :suit "♥"} {:rank 3 :suit "♣"} {:rank 4 :suit "♦"}])
+           9))))
+
+(deftest score-with-face-card
+  (testing "That we can score a hand with a face card in it."
+    (is (= (score-hand [{:rank "jack" :suit "♥"} {:rank 2 :suit "♣"}])
+           12))))
+
+(deftest score-ace-two
+  (testing "That we can score a hand with a high ace."
+    (is (= (score-hand [{:rank "ace" :suit "♥"} {:rank 2 :suit "♣"}])
+           13))))
+
+(deftest score-ace-10
+  (testing "That we can score a hand with a low ace.")
+  (is (= (score-hand [{:rank "ace" :suit "♥"} {:rank 9 :suit "♣"} {:rank 2 :suit "♦"}])
+         12)))
+
+(deftest score-10-ace
+  (testing "That we can score a hand with a low ace irrespective of order."))
+
+(deftest score-ace-ace)
