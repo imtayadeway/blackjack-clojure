2addced8120b2f3826cc11999c201768b12a2d49
Tim Wade <hello@timjwade.com>
2017-03-10 18:30:34 -0800

    Green!

--- src/blackjack_clojure/core.clj
+++ src/blackjack_clojure/core.clj
@@ -3,11 +3,38 @@
 (def suits #{"♠" "♥" "♦" "♣"})
 (def ranks (set (concat ["ace"] (range 2 11) ["jack" "queen" "king"])))
 (def deck
-  (into [] (for [suit suits
-                 rank ranks]
-             {:suit suit, :rank rank})))
+  (ref (into [] (for [suit suits
+                      rank ranks]
+                  {:suit suit, :rank rank}))))
+
+(def dealer-hand (ref []))
+(def player-hand (ref []))
+
+(defn deal
+  [hand deck]
+  (dosync
+   (alter hand concat @hand (take 2 @deck))
+   (ref-set deck (vec (drop 2 @deck)))))
+
+(defn shuffle-deck
+  [deck]
+  (dosync (alter deck shuffle)))
+
+(defn play-round
+  []
+  (shuffle-deck deck)
+  (deal player-hand deck)
+  (deal dealer-hand deck))
 
 (defn card-value
+  [card accumulative-score]
+  (let [rank (:rank card)]
+    (case rank
+      (2 3 4 5 6 7 8 9 10) rank
+      ("jack" "queen" "king") 10
+      "ace" (if (> accumulative-score 10) 1 11))))
+
+(defn high-value
   [card]
   (let [rank (:rank card)]
     (case rank
@@ -15,14 +42,15 @@
       ("jack" "queen" "king") 10
       "ace" 11)))
 
-(defn any-aces? [values] (some #(= 11 %) values))
+(defn recursive-score
+  [hand accumulative-score]
+  (if (empty? hand)
+    accumulative-score
+    (recur (rest hand)
+           (+ accumulative-score
+              (card-value (first hand) accumulative-score)))))
 
-(defn score-values
-  [values]
-  (let [sorted-values (-> values (sort) (vec))
-        sum (reduce + values)]
-    (if (<= sum 21)
-      sum
-      (if (any-aces? values)
-        (recur (-> sorted-values (pop) (conj 1)))
-        sum))))
+(defn score-hand
+  [hand]
+  (let [sorted-hand (sort-by high-value hand)]
+    (recursive-score sorted-hand 0)))
--- test/blackjack_clojure/core_test.clj
+++ test/blackjack_clojure/core_test.clj
@@ -12,48 +12,47 @@
 
 (deftest deck-count
   (testing "That we have 52 cards in the deck."
-    (is (= (count deck) 52))))
-
-(deftest numbered-card-value
-  (testing "That a numbered card returns its value"
-    (is (= 2 (card-value {:rank 2, :suit "♠"})))))
-
-(deftest face-card-value
-  (testing "That a face card returns a 10"
-    (is (= 10 (card-value {:rank "jack", :suit "♠"})))))
-
-(deftest ace-value
-  (testing "That aces are high"
-    (is (= 11 (card-value {:rank "ace", :suit "♠"})))))
+    (is (= (count (deref deck)) 52))))
 
 (deftest score-two-twos
   (testing "That we can score a hand with two twos."
-    (is (= 4 (score-values [2 2])))))
+    (is (= 4 (score-hand [{:rank 2, :suit "♠"},
+                          {:rank 2, :suit "♥"}])))))
 
 (deftest score-two-three
   (testing "That we can score a hand with a two and a three."
-    (is (= 5 (score-values [2 3])))))
+    (is (= 5 (score-hand [{:rank 2, :suit "♠"},
+                          {:rank 3, :suit "♠"}])))))
 
 (deftest score-two-three-four
   (testing "That we can score a hand with three cards."
-    (is (= 9 (score-values [2 3 4])))))
+    (is (= 9 (score-hand [{:rank 2, :suit "♠"},
+                          {:rank 3, :suit "♠"},
+                          {:rank 4, :suit "♠"}])))))
 
 (deftest score-with-face-card
   (testing "That we can score a hand with a face card in it."
-    (is (= 12 (score-values [10 2])))))
+    (is (= 12 (score-hand [{:rank 10, :suit "♠"},
+                           {:rank 2, :suit "♠"}])))))
 
 (deftest score-ace-two
   (testing "That we can score a hand with a high ace."
-    (is (= 13 (score-values [11 2])))))
+    (is (= 13 (score-hand [{:rank "ace", :suit "♠"},
+                           {:rank 2, :suit "♠"}])))))
 
 (deftest score-ace-11
   (testing "That we can score a hand with a low ace.")
-  (is (= 12 (score-values [11 9 2]))))
+  (is (= 12 (score-hand [{:rank "ace", :suit "♠"},
+                         {:rank 9, :suit "♠"},
+                         {:rank 2, :suit "♠"}]))))
 
 (deftest score-11-ace
   (testing "That we can score a hand with a low ace irrespective of order."
-    (is (= 12 (score-values [9 2 11])))))
+    (is (= 12 (score-hand [{:rank 9, :suit "♠"},
+                           {:rank 2, :suit "♠"},
+                           {:rank "ace", :suit "♠"}])))))
 
 (deftest score-ace-ace
   (testing "That we can score a hand with one high and one low ace."
-    (is (= 12 (score-values [11 11])))))
+    (is (= 12 (score-hand [{:rank "ace", :suit "♠"},
+                           {:rank "ace", :suit "♥"}])))))
