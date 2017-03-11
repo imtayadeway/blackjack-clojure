66cb4d85cd49facdf98d947712446c67242ab0cd
Tim Wade <hello@timjwade.com>
2017-03-10 18:29:52 -0800

    Upgrade cider

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]])
+  :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]])
