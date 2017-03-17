f5b6e4083bbdf31e7133f45793271d502e1acbb8
Tim Wade <hello@timjwade.com>
2017-03-17 11:29:36 -0700

    Default ns for lein repl

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :repl-options {:init-ns blackjack-clojure.core}
   :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]])
